f1ebb233465404b45acafc612e7840d593bf188b
Lydia Bleifuss <lbleifuss@bren.ucsb.edu>
2020-01-29 10:06:48 -0800

    finished violin graph example!

--- app4.R
+++ app4.R
@@ -6,6 +6,7 @@ library(shinythemes)
 library(here)
 
 ui <- navbarPage("Allison's navigation bar!",
+                 theme = shinytheme("cyborg"),
                  tabPanel("First tab!", 
                           h1("Some giant text"),
                           p("Here's some regular text..."),
@@ -16,19 +17,34 @@ ui <- navbarPage("Allison's navigation bar!",
                                          checkboxGroupInput(inputId = "diamondclarity",
                                                             "Choose some!",
                                                             choices = c(levels(diamonds$clarity)))),
-                            mainPanel("Main panel text.")
+                            mainPanel("Main panel text.",
+                                      plotOutput(outputId = "diamond_plot_2"))
                           ))
                  )
 
+
+
 server <- function(input, output) {
   
+  diamond_clarity <- reactive({
+    diamonds %>% 
+      filter(clarity %in% input$diamondclarity)
+  })
+  
   output$diamond_plot <- renderPlot({
-    
     ggplot(data = diamonds, aes(x = carat, y = price)) +
       geom_point(aes(color = clarity))
-    
   })
+  
+  
+  output$diamond_plot_2 <- renderPlot({
+    ggplot(data = diamond_clarity(), aes(x = clarity, y = price)) +
+      geom_violin(aes(fill = clarity))
+  })
+  
   
 }
 
+
 shinyApp(ui = ui, server = server)
+
